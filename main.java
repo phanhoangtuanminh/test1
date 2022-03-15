e47d3e39799232d75fd26b6bc994b78df30e6288
Phan Hoang Tuan Minh <20020302@vnu.edu.vn>
2022-03-15 22:48:31 +0700

    test1

--- main.java
+++ main.java
@@ -2,6 +2,6 @@ import java.sql.SQLOutput;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world1");
     }
 }
